57dcef0169b9e5cea362047ef87b1238df481fc6
tetiana <tetianayaremko@gmail.com>
2020-07-17 14:57:56 +0200

    shopping cart

---
+++ src/routes/cart/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const db = require("../../db");
+
+const router = express.Router();
+
+router.route("/").get(async (req, res) => {
+  try {
+    let result = await db.query(
+      `SELECT  row_to_json(row) FROM (select  COUNT("productId") as quantity, SUM(p.price) as total, p AS product
+       from carts inner join (select * from products) p
+        on "productId"=p._id group by ( "productId", p)) row`
+    );
+    let filter = result.rows.map((item) => item.row_to_json);
+    res.send(filter);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Internal server error");
+  }
+});
+router
+  .route("/:prodId")
+  .post(async (req, res) => {
+    try {
+      let result = await db.query(
+        `INSERT INTO public.carts( "productId")
+                                                    VALUES ($1)`,
+        [req.params.prodId]
+      );
+      if (result.rowCount === 1) res.send("ok");
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Internal server error");
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      let result = await db.query(
+        ` DELETE FROM public.carts WHERE _id in (select _id from carts where "productId"=$1 limit 1)`,
+        [req.params.prodId]
+      );
+      if (result.rowCount === 0) return res.status(404).send("Not found");
+
+      res.send("ok");
+    } catch (e) {
+      console.log(e);
+      res.status(500).send("Internal server error");
+    }
+  });
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const db = require("./db");
 const dotenv = require("dotenv");
 const productsRouter = require("./routes/products");
 const reviewsRouter = require("./routes/reviews");
+const cartRouter = require("./routes/cart");
 dotenv.config();
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/products", productsRouter);
 app.use("/reviews", reviewsRouter);
+app.use("/cart", cartRouter);
 app.listen(process.env.PORT, () => {
   console.log("running on ", process.env.PORT);
 });
